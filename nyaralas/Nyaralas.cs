3312b7ce24450b2166f165a748e66872a4b064ce
M3morui <vivi.santa0320@gmail.com>
2025-02-25 13:16:25 +0100

    Add files via upload

---
+++ nyaralas/Nyaralas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nyaralas
+{
+    internal class Nyaralas
+    {
+        public int sorszam;
+        public string nev;
+        public string szallas;
+        public int foglalas;
+        public int napszam;
+        public int ar;
+        public string kedvezmeny;
+
+        public Nyaralas(string sorszam, string nev, string szallas, string foglalas, string napszam, string ar, string kedvezmeny)
+        {
+            this.sorszam = int.Parse(sorszam);
+            this.nev = nev;
+            this.szallas = szallas;
+            this.foglalas = int.Parse(foglalas);
+            this.napszam = int.Parse(napszam);
+            this.ar = int.Parse(ar);
+            this.kedvezmeny = kedvezmeny;
+        }
+    }
+}
---
+++ nyaralas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace nyaralas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Nyaralas> nyaralasok = new List<Nyaralas>();
+            string[] sorok = File.ReadAllLines("nyaralas.txt");
+            foreach (var sor in sorok)
+            {
+                string[] adatok = sor.Split(',');
+                Nyaralas uj = new Nyaralas (adatok[0], adatok[1], adatok[2], adatok[3], adatok[4], adatok[5], adatok[6]);
+                nyaralasok.Add (uj);
+            }
+            foreach (var item in nyaralasok)
+            {
+                Console.WriteLine($"{item.sorszam}, {item.nev}, {item.szallas}, {item.foglalas}, {item.napszam}, {item.ar}, {item.kedvezmeny}");
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
