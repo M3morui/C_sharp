e12f9edd42a5a8cb120d44c4c79c594b6452272d
M3morui <vivi.santa0320@gmail.com>
2025-03-13 11:54:29 +0100

    Add files via upload

---
+++ Hellje/Kinalat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hellje
+{
+    internal class Kinalat
+    {
+        public int Kid;
+        public int Bid;
+        public int ml;
+        public string tipus;
+        public string iz;
+
+        public Kinalat(string Kid, string Bid, string ml, string tipus, string iz)
+        {
+            this.Kid = int.Parse(Kid);
+            this.Bid = int.Parse(Bid);
+            this.ml = int.Parse(ml);
+            this.tipus = tipus;
+            this.iz = iz;
+        }
+    }
+}
