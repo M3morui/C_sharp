6c767e1884346482f872ea235702ff43760ee20b
M3morui <vivi.santa0320@gmail.com>
2025-03-17 13:15:52 +0100

    Add files via upload

---
+++ gyak3/Bank.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak3
+{
+    internal class Bank
+    {
+        public string tulajdonos_nev;
+        public int azonosito;
+        public int pinkod;
+        public Bank(string tulajdonos_nev, int azonosito, int pinkod)
+        {
+            this.tulajdonos_nev = tulajdonos_nev;
+            this.azonosito = azonosito;
+            this.pinkod = pinkod;
+        }
+
+        public (string, int, int) TulajdonosAdatai()
+        {
+            Console.WriteLine("Adja meg a nevét");
+            string tulajdonos_nev = Console.ReadLine();
+            Console.WriteLine("Adja meg az azonosító számát");
+            int azonosito = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg a pinkódját");
+            int pinkod = int.Parse(Console.ReadLine());
+
+            return (tulajdonos_nev, azonosito, pinkod);
+        }
+    }
+}
---
+++ gyak3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank tulajdonos = new Bank("", 0, 0);
+            var(tulajdonos_nev, azonosito, pinkod) = tulajdonos.TulajdonosAdatai();
+            Console.WriteLine($"Név: {tulajdonos_nev}, Azonosító: {azonosito}, Pinkód: {pinkod}");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
