6ec5239eb4af52933478664b8b4f1f839ed64531
M3morui <vivi.santa0320@gmail.com>
2025-03-18 13:07:12 +0100

    Add files via upload

---
+++ Bank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kérjetek be nevet, azonositot(8) és pinkodot(4)
+            SzamlaTulajdonos tulajdonos = new SzamlaTulajdonos("", 0, 0);
+            var (tulajdonos_nev, azonosito, pinkod) = tulajdonos.TulajdonosAdatai();
+            Create.TulajdonosAdatFeltolt(tulajdonos_nev, azonosito, pinkod);
+            Console.WriteLine("Sikeres volt az adatfeltöltés.");
+            Console.ReadKey();
+        }
+    }
+}
