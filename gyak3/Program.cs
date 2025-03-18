2bd54a29b5ef74b104dc12ae1127b8b1ee41ceca
M3morui <vivi.santa0320@gmail.com>
2025-03-18 13:07:38 +0100

    Add files via upload

--- gyak3/Program.cs
+++ gyak3/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,13 +10,6 @@ namespace gyak3
     {
         static void Main(string[] args)
         {
-            Bank tulajdonos = new Bank("", 0, 0);
-            var(tulajdonos_nev, azonosito, pinkod) = tulajdonos.TulajdonosAdatai();
-            Console.WriteLine($"Név: {tulajdonos_nev}, Azonosító: {azonosito}, Pinkód: {pinkod}");
-
-
-
-            Console.ReadKey();
         }
     }
 }
