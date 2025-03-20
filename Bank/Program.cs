d1990e08a88fba5d945a3dd1d6c0413b3846d782
M3morui <vivi.santa0320@gmail.com>
2025-03-20 14:22:38 +0100

    Add files via upload

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -10,11 +10,66 @@ namespace Bank
     {
         static void Main(string[] args)
         {
-            //Kérjetek be nevet, azonositot(8) és pinkodot(4)
-            SzamlaTulajdonos tulajdonos = new SzamlaTulajdonos("", 0, 0);
-            var (tulajdonos_nev, azonosito, pinkod) = tulajdonos.TulajdonosAdatai();
-            Create.TulajdonosAdatFeltolt(tulajdonos_nev, azonosito, pinkod);
-            Console.WriteLine("Sikeres volt az adatfeltöltés.");
+            int azonositas = 0;
+            int pinkodmegad = 0;
+            string tulajdonos_neve = "";
+            string szamlaszama = "";
+            decimal osszeg = 0;
+
+
+            Console.WriteLine("Nálunk bankol már?");
+            string valasz = Console.ReadLine();
+
+            if (valasz == "nem")
+            {
+                SzamlaTulajdonos tulajdonos = new SzamlaTulajdonos("", 0, 0);
+                var (tulajdonos_nev, azonosito, pinkod) = tulajdonos.TulajdonosAdatai();
+                int tulajdonos_id = Create.TulajdonosAdatFeltolt(tulajdonos_nev, azonosito, pinkod);
+                Console.WriteLine("Sikeres volt az adatfeltöltés.");
+
+                Szamla szamla = new Szamla();
+                var (szamlaszam, egyenleg) = szamla.SzamlaLetrehozas();
+                Create.SzamlaFeltolt(tulajdonos_id, szamlaszam, egyenleg);
+                Console.WriteLine("A számla sikeresen létre lett hozva.");
+
+
+            }
+            else
+            {
+                if (Ellenorzes.AdatEllenorzes(tulajdonos_neve, azonositas, pinkodmegad))
+                {
+                    Console.WriteLine("Sikeresen belépett a fiókjába.");
+                    Console.WriteLine($"Melyik műveletet válassza:\n 1.Egyenleg lekérés, \n2.Befizetés, \n3.Pénz felvétel");
+                    string muvelet = Console.ReadLine();
+                    switch (muvelet)
+                    {
+                        case "1":
+                            {
+                                SzamlaMuveletek.EgyenlegLekeres();
+                                break;
+                            }
+                        case "2":
+                            {
+                                SzamlaMuveletek.Befizetes(szamlaszama, osszeg);
+                                break;
+                            }
+                        case "3":
+                            {
+                                SzamlaMuveletek.Penzfelvetel(szamlaszama, osszeg);
+                                break;
+                            }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Hibás adatot adott meg.");
+                }
+            }
+
+
+
+
+
             Console.ReadKey();
         }
     }
