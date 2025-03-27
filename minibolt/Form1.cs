dea83a5a1c2ed1e7237c8587c4ad947acc6d06dc
M3morui <vivi.santa0320@gmail.com>
2025-03-27 12:37:43 +0100

    Add files via upload

---
+++ minibolt/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace minibolt
+{
+    public partial class Form1 : Form
+    {
+
+        public decimal db1 = 0;
+        public decimal price1 = 0;
+        public decimal db2 = 0;
+        public decimal price2 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            db1 = numericUpDown1.Value;
+            price1 = Convert.ToDecimal(label1.Text);
+            MessageBox.Show("Kosárba raktad a termékeket!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            db2 = numericUpDown2.Value;
+            price2 = Convert.ToDecimal(label2.Text);
+            MessageBox.Show("Kosárba raktad a termékeket!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            decimal result = (db1 * price1) + (db2 * price2);
+            MessageBox.Show($"Köszönjük a vásárlást! A Végösszeg: {result}");
+        }
+    }
+}
