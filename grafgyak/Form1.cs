2f522cc586f27ebbd2ef058bdbf4911ded761081
M3morui <vivi.santa0320@gmail.com>
2025-03-27 12:25:02 +0100

    Add files via upload

---
+++ grafgyak/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace grafgyak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ar = 360;
+            int classsic = Convert.ToInt32(textBox1.Text);
+            int cherry = Convert.ToInt32(textBox2.Text);
+            int redorange = Convert.ToInt32(textBox3.Text);
+
+            int osszeg = 360 * (cherry + classsic + redorange);
+
+            label5.Text = $"Összesen {osszeg} Ft lesz a fizetendő összeg.";
+        }
+    }
+}
