dcb3d71dfa6ec1b06d11de6af9b4e7ff94aa66a0
M3morui <vivi.santa0320@gmail.com>
2025-05-08 14:05:44 +0200

    Add files via upload

---
+++ fajlgyak2/Loverseny.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fajlgyak2
+{
+    internal class Loverseny
+    {
+        public string zsoke;
+        public string lonev;
+        public bool nyert;
+        public int nyeremeny;
+        public int helyezes;
+
+        public Loverseny(string zsoke, string lonev, string nyert, string nyeremeny, string helyezes)
+        {
+            this.zsoke = zsoke;
+            this.lonev = lonev;
+            this.nyert = bool.Parse(nyert);
+            this.nyeremeny = int.Parse(nyeremeny);
+            this.helyezes = int.Parse(helyezes);
+        }
+    }
+}
